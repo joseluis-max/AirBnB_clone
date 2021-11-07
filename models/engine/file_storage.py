23e4c2153bb5e6230812f742a51a3f746fa180e7
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-11-06 19:10:25 -0500

    UPDATE: file storage imports

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,8 @@ from models.city import City
 from models.user import User
 from models.amenity import Amenity
 from models.place import Place
+from models.state import State
+from models.review import Review
 
 
 class FileStorage:
